4353bf11c65ef9dce05d7797f039dcc207b84196
Dave Enrico <denrico@northbergen.k12.nj.us>
2018-02-13 10:36:06 -0500

    Add task_01 to assignments/

---
+++ assignments/task_01/task_01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Create an object called number that stores the value 7
+number = 7
+
+# Create an object called my_list that stores the list of numbers from 1 to 10 inclusive.
+
+
+# Use list slicing on my_list to store the list [1, 2, 3] under the name easy_as
+
+
+# Define a function called reverse that accepts a string as input and returns that string in reverse.
+
+
+# Define a function called is_palindrome that accepts a string as input and returns True if that string reads the same backwards and forwards, and False otherwise.
+# Example: is_palindrome("racecar") should return True. is_palindrome("minivan") should return False.
+# You may assume that all characters will be lowercase.
+
+
