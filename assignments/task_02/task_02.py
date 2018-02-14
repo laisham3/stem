b34cd41145c08d6550bf6523bbb24ec87f09fed7
Dave Enrico <denrico@northbergen.k12.nj.us>
2018-02-14 11:35:56 -0500

    Change instructions of task_02.py

--- assignments/task_02/task_02.py
+++ assignments/task_02/task_02.py
@@ -1,2 +1,7 @@
-# Write code that opens the file numbers.txt and stores the sum of the numbers within to a variable called "total"
-# numbers.txt contains integers separated by newlines.
+# Write a function called total that takes a filename as its only argument and returns the sum of the numbers in that file. 
+# You may assume that the file is formatted in such a way that each number is an integer and occupies its own line.
+# You may not assume you know how many numbers are in the file.
+
+# The file numbers.txt in this directory follows these guidelines.
+
+
