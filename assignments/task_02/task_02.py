1ac37ba6451ac99046d9087bbbeef453fb4f8ae2
me <me@me.com>
2018-02-14 12:06:24 -0500

    made a function that adds the sum of everything

--- assignments/task_02/task_02.py
+++ assignments/task_02/task_02.py
@@ -1,7 +1,9 @@
-# Write a function called total that takes a filename as its only argument and returns the sum of the numbers in that file. 
+# Write a function called total that takes a filename as its only argument and returns the sum of the numbers in that file.
+def total(filename):
+    return 2+2
+
+print()
 # You may assume that the file is formatted in such a way that each number is an integer and occupies its own line.
 # You may not assume you know how many numbers are in the file.
 
 # The file numbers.txt in this directory follows these guidelines.
-
-
